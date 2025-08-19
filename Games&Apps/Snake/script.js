88a77927b22e04d9cfae35c91c3fce15e8aa383b
bolormau <bolormau@gmail.com>
2025-08-19 14:48:33 +0800

    started snake in games

---
+++ Games&Apps/Snake/script.js
@@ -0,0 +1,49 @@
+// ==============================================
+// =================== SNAKE ====================
+// ==============================================
+
+// To do: ------------------------------
+// score
+// winner
+// rotate or die when chundul to frame
+// buttons
+
+// set interval
+// random
+// f to switch direction
+// input to take direction
+// dir === y -> left(), right(); dir = x
+// dir === x -> up(), down(); dir = y
+// make wall scalable (with breaks)
+// always save rotated position
+// event listener
+// 34sqr - h
+
+// ===============
+
+let headX = 80;
+let headY = 40;
+
+// ==========================
+// let dir; //
+// let axis; // x, y
+
+// let dirChanger = (dir) => {
+
+// }
+
+// addEventListener("press", (pressed) => {
+//   if(pressed.key === "ArrowDown")
+// })
+
+// Go Function
+// function go(dir, isYAxis) {
+//   if(isYAxis) {
+//     if(dir === right) { turn right; dir = right; isYAxis = !isYAxis; //false }
+//     else if(dir === left) { turn left; dir = left; isYAxis = !isYAxis; }
+//   }
+//   else {
+//     if(dir === up) { turn up; dir = up; isYAxis = !isYAxis; //true }
+//     else if(dir === down) { turn right; dir = down; isYAxis = !isYAxis; }
+//   }
+// }
