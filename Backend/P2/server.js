37e46ae066545d6ec6f63c81d02a2333e366c839
bolormau <bolormau@gmail.com>
2025-08-19 11:01:24 +0800

    p2(p1) add

---
+++ Backend/P2/server.js
@@ -0,0 +1,44 @@
+// const fs = require('fs');
+
+// // Relative Path - Haritsangui
+
+// fs.writeFileSync('test.txt', "Edit #1", 'utf8');
+
+// fs.readFile('test.txt', 'utf8', (err, data) => {
+//   console.log("This is the file content: ", data);
+// })
+
+// // Absolute Path - Unemlehui?~
+// .....
+
+// ./  => out from file
+// ../ => out from folder
+
+// import
+
+// import fs from "fs";
+// import { calculateSum, print } from "./sum.js";
+
+// // Relative path
+// fs.readFile("./test.txt", "utf8", (err, data) => {
+//   console.log(data);
+// });
+
+// // Absolute path
+// fs.readFile(
+//   "/Users/25LP4130/Documents/Practices/P5/test.txt",
+//   "utf",
+//   (err, data) => {
+//     if (err) {
+//       console.error("Error reading file: ", err);
+//       return;
+//     }
+
+//     console.log(data);
+//   }
+// );
+
+// calculateSum(10, 20);å
+
+
+console.log(chalk.pink("Hi, I'm rainbow"));
---
+++ Backend/P2/sum.js
@@ -0,0 +1,8 @@
+export function calculateSum(a, b) {
+  const sum = a + b;
+  print(sum);
+}
+
+function print(result) {
+  console.log(result);
+}
